5984065beaeaed8fd1ca11690f927e112459d37c
bjrichards <braedenrichards@nevada.unr.edu>
2018-11-28 15:01:46 -0800

    Added more logging

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ class Node:
         LogFile.write("\tNode ID: ")
         LogFile.write(str(self.nodeID))
         LogFile.write("\n\tNode # Connections: ")
-        LogFile.write(str(self.interfaces))
+        LogFile.write(str(self.numberOfInterfaces))
         LogFile.write("\n\tNodes Connected: ")
         LogFile.write(str(self.interfaces))
         LogFile.write("\n\tInterface Weights: ")
@@ -113,39 +113,61 @@ class Node:
 
 # @Desc: Reads the nodal information from the file and creates a list of nodes
 #        that make up the network
-# @Param: node_file <string>: the file path and name to open
+# @Param: nodeFile <string>: the file path and name to open
 # @Return: nodes <List[node]>: List of nodes
 def LoadNodeNetwork(nodeFile):
-    node_file = open(NodeFile, 'r')
+    # Logging the opening of the node file
+    LogFile.write("Opening Node file: ")
+    LogFile.write(nodeFile)
+    LogFile.write("\n")
+    node_file = open(nodeFile, 'r') # Open node file
+
     soup = node_file.read()
     slightly_less_soup = soup.split("\n")
     nodes = []
+
+    # Parse the soup for node data, and create node objects with that data
     for i in range(1, int(slightly_less_soup[0]) + 1):
         node = slightly_less_soup[i].split(" ")
         nodeID = int(node[0])
         nodeNumConnections = int(node[1])
         otherNodes = []
         otherWeights = []
+
         for j in range(1, nodeNumConnections + 1):
             group = node[j+1].split(":")
             otherNodes.append(group[0])
             otherWeights.append(group[1])
+
+        # Log which node is being Created
         LogFile.write("Creating Node: ")
         LogFile.write(str(nodeID))
+        LogFile.write("\n")
         print("Creating Node: ", nodeID)
+
+        # Create Node Object
         newNode = Node(nodeID, nodeNumConnections, otherNodes, otherWeights)
         nodes.append(newNode)
 
+        # Log the node was created
+        LogFile.write("Node ")
+        LogFile.write(str(nodeID))
+        LogFile.write(" created\n")
+        print("Node ", nodeID, " created")
+
+    LogFile.write("Closing file\n\n")
+    node_file.close()
     return nodes
 
-# Path and name to file containing the nodes of the network
-NodeFile = "nodes.nd"
+# File name to log to
 LogFileName = "log.log"
-LogFile = open(LogFileName, 'w')
+LogFile = open(LogFileName, 'w') # Opening of the log file
 
 def main():
+
+    node_file = input("Enter the file to load node data from: ")
     # Load in nodes to program
-    nodes = LoadNodeNetwork(NodeFile)
+    nodes = LoadNodeNetwork(node_file)
 
     return
 
